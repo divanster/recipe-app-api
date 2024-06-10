6477194380c21595b9c143188500267802fa36c8
divanster <divanster@gmail.com>
2024-06-10 15:24:15 +0300

    addind API and Swagger

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -30,7 +31,6 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
-
 
     def test_new_user_without_email_raises_error(self):
         """Test creating user with no email raises a ValueError"""
